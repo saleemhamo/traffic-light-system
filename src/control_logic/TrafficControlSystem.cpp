af311d5931123718dbcf7177d39a87e9b7880fe4
saleemhamo <saleemhamo@gmail.com>
2024-03-20 21:40:15 +0200

    refine project structure

---
+++ src/control_logic/TrafficControlSystem.cpp
@@ -0,0 +1,83 @@
+//
+// Created by Saleem Hamo on 12/02/2024.
+//
+
+#include "control_logic/TrafficControlSystem.h"
+
+TrafficControlSystem::TrafficControlSystem() : currentState(State::CAR_RED_PEDESTRIAN_GREEN) {
+    // Initialize system, possibly setting initial states of lights, etc.
+    updateOutputs(); // Ensure outputs match the initial state
+}
+
+// TODO: Create and move to system handler, create system handler interface and add it  to this system and the cars traffic light system
+void TrafficControlSystem::run() {
+
+}
+
+void TrafficControlSystem::update() {
+    /*
+     * This method would be called in a loop to regularly check for state transitions
+     * and possibly read sensors if not interrupt-driven
+     */
+}
+
+
+void TrafficControlSystem::changeState(State newState) {
+    currentState = newState;
+    updateOutputs(); // Update outputs anytime the state changes
+}
+
+void TrafficControlSystem::initializeSystemState() {
+    updateOutputs(); // Update outputs anytime the state changes
+}
+
+
+void TrafficControlSystem::updateOutputs() {
+    // Based on the current state, control the traffic lights, buzzer, and warning light
+    // This is a placeholder for actual GPIO control logic
+    switch (currentState) {
+        case State::CAR_RED_PEDESTRIAN_GREEN:
+            // Set car light to red, pedestrian light to green, disable buzzer and warning light
+            break;
+        case State::CAR_GREEN_PEDESTRIAN_RED:
+            // Set car light to green, pedestrian light to red, disable buzzer and warning light
+            break;
+        case State::CAR_GREEN_PEDESTRIAN_WAIT:
+            // Keep car light green, pedestrian light red, enable warning light, maybe enable buzzer shortly
+            break;
+        case State::CAR_RED_PEDESTRIAN_GREEN_SAFE:
+            // Set car light to red, pedestrian light to green, disable buzzer, and warning light
+            break;
+            // Handle other states as needed
+    }
+}
+
+void TrafficControlSystem::handleCarTrafficLightChange(bool isGreen) {
+    if (isGreen && currentState == State::CAR_RED_PEDESTRIAN_GREEN) {
+        changeState(State::CAR_GREEN_PEDESTRIAN_RED);
+    } else if (!isGreen && currentState == State::CAR_GREEN_PEDESTRIAN_RED) {
+        changeState(State::CAR_RED_PEDESTRIAN_GREEN);
+    }
+}
+
+void TrafficControlSystem::handleRoadStatusChange(bool isEmpty) {
+    if (isEmpty && currentState == State::CAR_GREEN_PEDESTRIAN_WAIT) {
+        changeState(State::CAR_RED_PEDESTRIAN_GREEN_SAFE);
+    }
+    // Add logic for other scenarios as necessary
+}
+
+void TrafficControlSystem::handlePedestrianButtonPress() {
+    if (currentState == State::CAR_GREEN_PEDESTRIAN_RED) {
+        changeState(State::CAR_GREEN_PEDESTRIAN_WAIT);
+    }
+    // Adjust based on system requirements
+}
+
+void TrafficControlSystem::handlePedestrianSensorTrigger(bool isDetected) {
+    // Implement according to how sensor detection affects the system state
+}
+
+TrafficControlSystem::State TrafficControlSystem::getCurrentState() {
+    return TrafficControlSystem::State::CAR_RED_PEDESTRIAN_GREEN;
+}
---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file main.cpp
+ * @brief This is the main file of the project
+ */
+
+#include "main/MainSystem.h"
+#include "utils/Logger.h"
+#include <iostream>
+#include <string>
+#include <chrono>  // For std::chrono
+#include <thread>  // For std::this_thread
+
+void init();
+
+void close();
+
+int main() {
+    init();
+    MainSystem mainSystem;
+    mainSystem.initialize();
+    mainSystem.runSystems();
+
+    // Keep the application running in a loop
+    std::string command;
+    while (true) {
+        // Here, for simplicity, we just sleep and keep the program alive
+        // In a real application, you might check for input to shut down,
+        // monitor system status, or perform other background tasks.
+//        std::this_thread::sleep_for(std::chrono::seconds(1));  // Sleep for a bit to reduce CPU usage
+
+        // Example: Check for a termination command or condition
+        std::cout << "Type 'exit' to stop the system: ";
+        std::getline(std::cin, command);
+        if (command == "exit") {
+            break;  // Exit the loop if 'exit' command is given
+        }
+    }
+
+    // Implement any necessary cleanup before shutting down
+    // Note: Depending on your system's requirements, you might need to properly
+    // shut down subsystems or handle other cleanup tasks here.
+
+    close();
+    return 0;
+}
+
+void init() {
+    try {
+        Logger::init("app.log"); // Adjust the file name as needed
+        Logger::logInfo("Application starting");
+        // The rest of your program
+    } catch (const std::exception &e) {
+        std::cerr << "Failed to initialize logger: " << e.what() << std::endl;
+    }
+}
+
+void close() {
+    Logger::close();
+}
