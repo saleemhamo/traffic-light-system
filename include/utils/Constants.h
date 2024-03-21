112eeff0927cc2ca90cc4185e551093145cf5a1f
AlexandruRO45 <savaalexandru562@gmail.com>
2024-03-21 20:21:14 +0000

    Fix pin assignments in Constants.h

--- include/utils/Constants.h
+++ include/utils/Constants.h
@@ -9,18 +9,21 @@ namespace Constants {
     constexpr int PedestriansTrafficLightRedPin = 7;
     constexpr int PedestriansTrafficLightGreenPin = 11;
     constexpr int CarsTrafficLightRedPin = 29;
-//    constexpr int CarsTrafficLightRedPin = 27; // TODO: yellow PIN CORRECTED
+    // constexpr int CarsTrafficLightRedPin = 27; // TODO: yellow PIN CORRECTED
     constexpr int CarsTrafficLightGreenPin = 28;
+    // Traffic ultrasonic
     constexpr int CheckingSystemUltrasonic1TriggerPin = 30;
-    constexpr int CheckingSystemUltrasonic1EchoPin = 31; // Traffic ultrasonic
+    constexpr int CheckingSystemUltrasonic1EchoPin = 31; 
+    // Pedestrian ultrasonic
     constexpr int CheckingSystemUltrasonic2TriggerPin = 3;
-    constexpr int CheckingSystemUltrasonic2EchoPin = 2; //Pedestrian ultrasonic
-    constexpr int CheckingSystemButtonPin = 21; //push botton
-    constexpr int WarningSystemLedPin = 25; //25 24 23
+    constexpr int CheckingSystemUltrasonic2EchoPin = 2; 
+    constexpr int CheckingSystemButtonPin = 21;
+    // All three warning system leds are connected to 25, 24 and 23
+    constexpr int WarningSystemLedPin = 25; 
+    constexpr int WarningSystemLed2Pin = 24;
+    constexpr int WarningSystemLed3Pin = 23;
     constexpr int WarningSystemBuzzerPin = 14;
     // Add more constants here
-    // buzzer 14
-    // 
 }
 
 #endif // End of the header guard
