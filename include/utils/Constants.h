997298701735402eb8d84363bf9c68d22ae5c92d
AlexandruRO45 <savaalexandru562@gmail.com>
2024-03-21 18:53:05 +0000

    Updated pin assignments and constants in Constants.h

--- include/utils/Constants.h
+++ include/utils/Constants.h
@@ -9,16 +9,18 @@ namespace Constants {
     constexpr int PedestriansTrafficLightRedPin = 7;
     constexpr int PedestriansTrafficLightGreenPin = 11;
     constexpr int CarsTrafficLightRedPin = 29;
-//    constexpr int CarsTrafficLightRedPin = 1; // TODO: yellow
-    constexpr int CarsTrafficLightGreenPin = 18;
-    constexpr int CheckingSystemUltrasonic1TriggerPin = 23;
-    constexpr int CheckingSystemUltrasonic1EchoPin = 23;
-    constexpr int CheckingSystemUltrasonic2TriggerPin = 23;
-    constexpr int CheckingSystemUltrasonic2EchoPin = 23;
-    constexpr int CheckingSystemButtonPin = 25;
-    constexpr int WarningSystemLedPin = 12;
-    constexpr int WarningSystemBuzzerPin = 18;
+//    constexpr int CarsTrafficLightRedPin = 27; // TODO: yellow PIN CORRECTED
+    constexpr int CarsTrafficLightGreenPin = 28;
+    constexpr int CheckingSystemUltrasonic1TriggerPin = 30;
+    constexpr int CheckingSystemUltrasonic1EchoPin = 31; // Traffic ultrasonic
+    constexpr int CheckingSystemUltrasonic2TriggerPin = 3;
+    constexpr int CheckingSystemUltrasonic2EchoPin = 2; //Pedestrian ultrasonic
+    constexpr int CheckingSystemButtonPin = 21; //push botton
+    // constexpr int WarningSystemLedPin = -; //25 24 23
+    constexpr int WarningSystemBuzzerPin = 14;
     // Add more constants here
+    // buzzer 14
+    // 
 }
 
 #endif // End of the header guard
