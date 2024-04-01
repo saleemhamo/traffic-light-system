39bb558901003addd04a6288f2dad3c53a3d2d00
saleemhamo <saleemhamo@gmail.com>
2024-04-01 22:21:29 +0300

    fix imports

--- src/sensors/PushButton.cpp
+++ src/sensors/PushButton.cpp
@@ -1,11 +1,10 @@
 #include "sensors/PushButton.h"
+#include <iostream>
 
-PushButton::PushButton(int pin) : gpioPin(pin)
-{
+PushButton::PushButton(int pin) : gpioPin(pin) {
 }
 
-PushButton::~PushButton()
-{
+PushButton::~PushButton() {
 #ifdef RASPBERRY_PI
     // Reset ISR handling on this pin
     gpioSetAlertFunc(gpioPin, NULL);
@@ -13,8 +12,7 @@ PushButton::~PushButton()
     // Optional: gpioTerminate(); // Consider if you want to terminate pigpio on destruction. Beware if using multiple GPIO devices.
 }
 
-void PushButton::initialize()
-{
+void PushButton::initialize() {
     // Initialize pigpio and set pin modes, only on Raspberry Pi
 #ifdef RASPBERRY_PI
     if (gpioInitialise() < 0)
@@ -30,18 +28,15 @@ void PushButton::initialize()
     // More initialization as necessary
 }
 
-void PushButton::registerButtonPressCallback(ButtonCallback callback)
-{
+void PushButton::registerButtonPressCallback(ButtonCallback callback) {
     this->buttonPressCallback = callback;
 }
 
-void PushButton::buttonPressHandler(int gpio, int level, uint32_t tick, void *user)
-{
+void PushButton::buttonPressHandler(int gpio, int level, uint32_t tick, void *user) {
     // This function is called in the context of a new thread when the button is pressed
     std::cout << "Button Pressed " << gpio << " " << level << " " << tick << std::endl;
     PushButton *button = static_cast<PushButton *>(user);
-    if (button->buttonPressCallback)
-    {
+    if (button->buttonPressCallback) {
         button->buttonPressCallback(); // Call the user's callback
     }
 }
