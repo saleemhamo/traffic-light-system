b6d0e66eea82f3173830f3b5705ff3e3d3f8464d
AlexandruRO45 <savaalexandru562@gmail.com>
2024-04-01 13:37:32 +0100

    Add button functionality

--- src/sensors/PushButton.cpp
+++ src/sensors/PushButton.cpp
@@ -1,36 +1,47 @@
 #include "sensors/PushButton.h"
 
-PushButton::PushButton(int pin) : gpioPin(pin) {
+PushButton::PushButton(int pin) : gpioPin(pin)
+{
 }
 
-PushButton::~PushButton() {
+PushButton::~PushButton()
+{
 #ifdef RASPBERRY_PI
     // Reset ISR handling on this pin
-    gpioSetISRFuncEx(gpioPin, FALLING_EDGE, 0, NULL, NULL);
+    gpioSetAlertFunc(gpioPin, NULL);
 #endif
     // Optional: gpioTerminate(); // Consider if you want to terminate pigpio on destruction. Beware if using multiple GPIO devices.
 }
 
-void PushButton::initialize() {
+void PushButton::initialize()
+{
     // Initialize pigpio and set pin modes, only on Raspberry Pi
 #ifdef RASPBERRY_PI
-    gpioInitialise();
+    if (gpioInitialise() < 0)
+    {
+        std::cerr << "Unable to connect PIGPIO, exiting" << std::endl;
+        return 1;
+    }
+    gpioSetMode(gpioPin, PI_INPUT);
     gpioSetPullUpDown(gpioPin, PI_PUD_UP);
     // Set ISR for falling edge (button press)
-    gpioSetISRFuncEx(gpioPin, FALLING_EDGE, 0, buttonPressHandler, this);
-
+    gpioSetAlertFunc(gpioPin, buttonPressHandler);
 #endif
     // More initialization as necessary
 }
 
-void PushButton::registerButtonPressCallback(ButtonCallback callback) {
+void PushButton::registerButtonPressCallback(ButtonCallback callback)
+{
     this->buttonPressCallback = callback;
 }
 
-void PushButton::buttonPressHandler(int gpio, int level, uint32_t tick, void *user) {
+void PushButton::buttonPressHandler(int gpio, int level, uint32_t tick, void *user)
+{
     // This function is called in the context of a new thread when the button is pressed
+    std::cout << "Button Pressed " << gpio << " " << level << " " << tick << std::endl;
     PushButton *button = static_cast<PushButton *>(user);
-    if (button->buttonPressCallback) {
+    if (button->buttonPressCallback)
+    {
         button->buttonPressCallback(); // Call the user's callback
     }
 }
